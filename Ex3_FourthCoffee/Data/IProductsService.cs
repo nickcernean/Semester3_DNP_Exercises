96207b9c4bd6b733f6945084ee64b6af1dee7adc
nickcernean <nicolaecer@gmail.com>
2021-10-03 23:51:20 +0200

    Session 2(5), Session3(3),Session4(1),Session5(2)

---
+++ Ex3_FourthCoffee/Data/IProductsService.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Ex3_FourthCoffee.Models;
+
+namespace Ex3_FourthCoffee.Data
+{
+    public interface IProductsService
+    {
+        public IList<Product> Products { get; }
+    }
+}
---
+++ Ex3_FourthCoffee/Models/Product.cs
@@ -0,0 +1,11 @@
+﻿namespace Ex3_FourthCoffee.Models
+{
+    public class Product
+    { public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string ImageName { get; set; }
+
+    }
+}
